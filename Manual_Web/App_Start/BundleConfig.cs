e0f4694e51b304bc5d291afdf61923aae1187101
Tharin Tee <Tharin.tee@gmail.com>
2019-12-15 23:15:13 +0700

    Start project!

--- Manual_Web/App_Start/BundleConfig.cs
+++ Manual_Web/App_Start/BundleConfig.cs
@@ -9,22 +9,46 @@ namespace Manual_Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
+                        "~/Scripts/jquery-{version}.js", 
+                        "~/Scripts/SkillHunt_js/jquery-migrate-3.0.1.min.js",
+                        "~/Scripts/SkillHunt_js/popper.min.js",
+                        "~/Scripts/SkillHunt_js/bootstrap.min.js",
+                        "~/Scripts/SkillHunt_js/jquery.easing.1.3.js",
+                        "~/Scripts/SkillHunt_js/jquery.waypoints.min.js",
+                        "~/Scripts/SkillHunt_js/jquery.stellar.min.js",
+                        "~/Scripts/SkillHunt_js/owl.carousel.min.js",
+                        "~/Scripts/SkillHunt_js/jquery.magnific-popup.min.js",
+                        "~/Scripts/SkillHunt_js/aos.js",
+                        "~/Scripts/SkillHunt_js/jquery.animateNumber.min.js",
+                        "~/Scripts/SkillHunt_js/scrollax.min.js",
+                        "~/Scripts/SkillHunt_js/main.js"));
+            
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
+            
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
-
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/SkillHunt_css/open-iconic-bootstrap.min.css",
+                      "~/Content/SkillHunt_css/animate.css",
+                      "~/Content/SkillHunt_css/owl.carousel.min.css",
+                      "~/Content/SkillHunt_css/owl.theme.default.min.css",
+                      "~/Content/SkillHunt_css/aos.css",
+                      "~/Content/SkillHunt_css/ionicons.min.css",
+                      "~/Content/SkillHunt_css/bootstrap-datepicker.css",
+                      "~/Content/SkillHunt_css/jquery.timepicker.css",
+                      "~/Content/SkillHunt_css/flaticon.css",
+                      "~/Content/SkillHunt_css/icomoon.css",
+                      "~/Content/SkillHunt_css/style.css",
+                      "~/Content/essence_css/core-style.css",
+                      "~/Content/essence_css/style.css"));
         }
     }
 }
--- Manual_Web/Controllers/HomeController.cs
+++ Manual_Web/Controllers/HomeController.cs
@@ -13,17 +13,13 @@ namespace Manual_Web.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Role()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
